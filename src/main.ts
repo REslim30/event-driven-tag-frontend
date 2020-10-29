cc8bdaa774549a88e9a3f2d39948d07f494feaf8
Aidan Do <giahuydo99@gmail.com>
2020-10-29 12:26:02 +1030

    Rxjs working

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-import * as io from "socket.io-client"
+/* import * as io from "socket.io-client" */
 
-var socket = io("3000");
+/* var socket = io("http://localhost:3000"); */
 
-socket.on("connect", (value: any) => {
-    console.log(value);
-});
+/* socket.on("connect", (value: any) => { */
+/*     console.log(value); */
+/* }); */
 
-socket.on("broadcast", (value: number) => {
-    console.log(value);
-});
+/* socket.on("broadcast", (value: number) => { */
+/*     console.log(value); */
+/* }); */
 
+/* console.log("hello-world"); */
+/* alert("hello"); */
+
+import * as Rx from "rxjs";
+
+Rx.interval(1000)
+    .subscribe((value: number) => {
+        alert(value);
+    });
