cec07c4631c5f8b5fdd32cece9f89dd5eafba3b3
Aidan Do <giahuydo99@gmail.com>
2020-10-29 12:43:56 +1030

    Sockets IO is working

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,12 @@
-/* import * as io from "socket.io-client" */
+import * as io from "socket.io-client"
 
-/* var socket = io("http://localhost:3000"); */
+const socket = io("http://192.168.1.28:3000");
 
-/* socket.on("connect", (value: any) => { */
-/*     console.log(value); */
-/* }); */
+socket.on("connect", (value: any) => {
+    console.log(value);
+});
 
-/* socket.on("broadcast", (value: number) => { */
-/*     console.log(value); */
-/* }); */
-
-/* console.log("hello-world"); */
-/* alert("hello"); */
-
-import * as Rx from "rxjs";
-
-Rx.interval(1000)
-    .subscribe((value: number) => {
-        alert(value);
-    });
+socket.on("broadcast", (value: number) => {
+    console.log(value);
+    alert(value);
+});
