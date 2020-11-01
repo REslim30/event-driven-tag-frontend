9472b59fa8c296f187a81cafeb5ef945f05223fa
Aidan Do <giahuydo99@gmail.com>
2020-11-01 11:36:38 +1030

    Tests started

---
+++ test/ClientFSMTest.js
@@ -0,0 +1,14 @@
+import { ClientFSM } from '../src/ClientFSM';
+let assert = require("assert");
+
+describe('ClientFSM', function() {
+  let client;
+
+  beforeEach(function() {
+    client = new ClientFSM();
+  });
+
+  it('Test', function() {
+    
+  });
+});
